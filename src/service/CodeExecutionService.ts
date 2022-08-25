91d654daec48385aeb16b17a9ce3e378647318ae
viveknathani <viveknathani2402@gmail.com>
2022-08-25 18:12:13 +0530

    chore: add progress, update paths, output file

--- src/service/CodeExecutionService.ts
+++ src/service/CodeExecutionService.ts
@@ -7,16 +7,7 @@ import { Job } from 'bullmq';
 import util from 'util';
 import path from 'path';
 
-
 const exec = util.promisify(require('child_process').exec);
-const helper = (error: any, stdout: any, stderr: any) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-}
 
 export class CodeExecutionService {
 
@@ -61,30 +52,44 @@ export class CodeExecutionService {
         try {
             const data: entity.JobQueueData = job.data;
             const jobId = job.name;
-            const executionAreaPath = `../execution-area`;
+            const executionAreaPath = `./execution-area`;
             const directoryPath = `${executionAreaPath}/${jobId}`;
             const submissionfileName = `submission.${this.getExtension(data.language)}`;
             const inputFileName = "input.txt";
-            const dockerFilePath = `../dockerfiles/${data.language}`;
+            const dockerFileParent = (process.env.NODE_ENV === 'test') ? `./src` : `./build`;
+            const dockerFilePath = `${dockerFileParent}/dockerfiles/${data.language}`;
             const submissionFilePath = path.resolve(`${directoryPath}/${submissionfileName}`);
             const inputFilePath = path.resolve(`${directoryPath}/${inputFileName}`);
+            const outputFilePath =  path.resolve(`${directoryPath}/output.txt`);
+
+            await job.updateProgress(entity.JobProgress.STARTED);
             await fs.promises.mkdir(directoryPath);
             await fs.promises.mkdir(`${directoryPath}/outputs`);
             await fs.promises.writeFile(submissionFilePath, data.code, {
                 encoding: 'utf-8'
             });
-            await fs.promises.writeFile(inputFilePath, this.getInputFileContent(data.testCases), {
+            await fs.promises.writeFile(inputFilePath, this.getFileContent(data.testCases, true), {
                 encoding: 'utf-8'
             });
-            await exec(`docker build -t ${jobId} -f ${dockerFilePath} --build-arg SUBMISSION_FILE_PATH=${submissionFilePath} --build-arg INPUT_FILE_PATH=${inputFilePath} ${executionAreaPath}`, helper);
-            await exec(`docker run -v ${path.resolve(directoryPath)}/outputs:/outputs ${jobId}`, helper);
-            //await exec(`docker rm ${jobId}`);
-            await this.processOutputs();
+            await fs.promises.writeFile(outputFilePath, this.getFileContent(data.testCases, false), {
+                encoding: 'utf-8'
+            });
+            await job.updateProgress(entity.JobProgress.MKDIR);
+            await exec(`docker build -t ${jobId} -f ${dockerFilePath} --build-arg SUBMISSION_FILE_PATH=${jobId}/${submissionfileName} --build-arg INPUT_FILE_PATH=${jobId}/${inputFileName} ${executionAreaPath}`);
+            await job.updateProgress(entity.JobProgress.DOCKER_BUILD);
+            await exec(`docker run -v ${path.resolve(directoryPath)}/outputs:/outputs ${jobId}`);
+            await job.updateProgress(entity.JobProgress.DOCKER_RUN);
+            await exec(`docker rmi -f ${jobId}`);
+            await job.updateProgress(entity.JobProgress.DOCKER_RMI);
+            await this.processOutputs(jobId, data.testCases);
+            await job.updateProgress(entity.JobProgress.PROCESS_OUTPUTS);
             await fs.promises.rm(directoryPath, {
                 recursive: true,
                 force: true
             });
+            await job.updateProgress(entity.JobProgress.RM);
         } catch (err) {
+            console.log(err);
             throw err;
         }
     }
@@ -96,15 +101,17 @@ export class CodeExecutionService {
         return language;
     }
 
-    private getInputFileContent(testCases: entity.TestCase[]): string {
+    private getFileContent(testCases: entity.TestCase[], writeInput: boolean): string {
         let result = `${testCases.length}\n`;
         for (let i = 0; i < testCases.length; ++i) {
-            result +=`${testCases[i].input}\n`;
+            result += (writeInput) ? `${testCases[i].input}\n` : `${testCases[i].output}\n`;
         }
         return result;
     }
 
-    private async processOutputs() {}
+    private async processOutputs(jobId: string, testCases: entity.TestCase[]) {
+        
+    }
 
     public async getJobResult(jobId: string) {
 
