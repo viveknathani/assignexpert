71eb7b5b6d3764bc7a2386f1dcbb4d872c25f3e7
viveknathani <viveknathani2402@gmail.com>
2022-08-25 12:03:02 +0530

    test: run job, pass args

---
+++ src/service/CodeExecutionService.test.ts
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import * as cache from '../cache';
+import { CodeExecutionService } from './CodeExecutionService';
+import * as entity from '../entity';
+dotenv.config();
+
+cache.setupCache(process.env.REDIS_URL || '', 
+                    process.env.REDIS_USER || '', process.env.REDIS_PWD || '');
+
+const codeExecutionService: CodeExecutionService = CodeExecutionService.getInstance();
+
+test('execute c', async () => {
+    const code = `
+        #include <stdio.h>
+        int main() {
+            int x, t;
+            scanf("%d", &t);
+            scanf("%d", &x);
+            printf("%d\\n", x);
+            return 0;
+        }
+    `;
+    const language = 'c';
+    const testCases: entity.TestCase[] = [
+        {
+            input: "5",
+            output: "5"
+        }
+    ];
+    codeExecutionService.runCode(code, language, testCases);
+});
