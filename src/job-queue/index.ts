16ae3d28640dbe995441ddfd07fb548f87994ca2
viveknathani <viveknathani2402@gmail.com>
2022-08-25 12:02:28 +0530

    fix: remove circular dependency

--- src/job-queue/index.ts
+++ src/job-queue/index.ts
@@ -1,6 +1,5 @@
-import { Queue, Worker } from 'bullmq';
+import { Queue, Worker, Job } from 'bullmq';
 import IORedis from 'ioredis';
-import { CodeExecutionService } from '../service/.';
 import * as entity from '../entity';
 
 const redisConnection = new IORedis(process.env.REDIS_URL || "");
@@ -9,9 +8,12 @@ const queue = new Queue('assignexpert', {
     connection: redisConnection
 });
 
-const codeExecutionService: CodeExecutionService = CodeExecutionService.getInstance();
-const worker = new Worker('assignexpert', codeExecutionService.job);
-
 export function addJob(jobId: string, data: entity.JobQueueData) {
     queue.add(jobId, data);
+}
+
+export function addWorker(job: (job: Job) => Promise<void>) {
+    const worker = new Worker('assignexpert', job, {
+        connection: redisConnection
+    });
 }
