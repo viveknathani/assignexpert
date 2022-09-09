c16755bcc956fbee0f61949a3d0fc5ab5f23dcc2
viveknathani <viveknathani2402@gmail.com>
2022-09-09 16:25:53 +0530

    chore: outsource execution to shell scripts

---
+++ src/scripts/c.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+gcc /ae/submission.c -o /ae/submission 2> /ae/compile.txt; 
+i=1
+while [ "$i" -le "$1" ]
+do
+   timeout $2 /usr/bin/time -f '%M-%e' -o /ae/stats$i.txt /ae/submission < /ae/input$i.txt > /ae/submission$i.txt 2> /ae/runtime$i.txt;
+   echo $? > /ae/timeout$i.txt; 
+   i=$(( i + 1 ))
+done
--- src/scripts/docker-build.sh
+++ src/scripts/docker-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+mkdir execution-area
+cp src/scripts/c.sh ./execution-area/
+cp src/scripts/cpp.sh ./execution-area/
+cp src/scripts/python.sh ./execution-area/
+cp src/scripts/java.sh ./execution-area/
 docker build -t assignexpert-c -f src/dockerfiles/c ./execution-area
 docker build -t assignexpert-cpp -f src/dockerfiles/cpp ./execution-area
 docker build -t assignexpert-python -f src/dockerfiles/python ./execution-area
