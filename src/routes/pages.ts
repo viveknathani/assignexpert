8c9b5c405d5de0dd86bcb34d83ae7670e82eb60f
viveknathani <viveknathani2402@gmail.com>
2022-09-27 22:21:12 +0530

    chore: serve landing page

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -2,13 +2,21 @@ import * as express from 'express';
 import path from 'path';
 const pageRouter: express.Router = express.Router();
 
-pageRouter.get('/run', async (req: express.Request, res: express.Response) => {
-    try {        
-        res.sendFile(path.resolve(__dirname,'../web/html/run.html'));
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: ''});
+type ExpressFunction = 
+        (req: express.Request, res: express.Response) => Promise<void>
+
+function directoryHandler(webPagePath: string): ExpressFunction {
+    return async (req: express.Request, res: express.Response) => {
+        try {        
+            res.sendFile(path.resolve(__dirname, webPagePath));
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: ''});
+        }
     }
-});
+}
+
+pageRouter.get('/', directoryHandler('../web/html/index.html'));
+pageRouter.get('/run', directoryHandler('../web/html/run.html'));
 
 export default pageRouter;
