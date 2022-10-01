05414f998dfb03a227a454f95ae0bcc2cdc10be7
viveknathani <viveknathani2402@gmail.com>
2022-10-01 12:57:14 +0530

    feat: serve class page via ejs

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -1,6 +1,9 @@
 import * as express from 'express';
 import { injectSessionInfoMiddleWare } from './user';
+import * as messages from './http_messages';
 import path from 'path';
+import { AssignmentService, ClassService, errors } from '../service';
+import * as ejs from 'ejs';
 
 const pageRouter: express.Router = express.Router();
 
@@ -24,5 +27,35 @@ pageRouter.get('/auth', directoryHandler('../web/html/authPage.html'));
 pageRouter.use(injectSessionInfoMiddleWare);
 pageRouter.get('/home', directoryHandler('../web/html/home.html'));
 pageRouter.get('/settings', directoryHandler('../web/html/settings.html'));
+pageRouter.get('/class', async (req: express.Request, res: express.Response) => {
+    try {
+        const classId = req.query['classId'] as string;
+        const { isStudent } = req.body;
+        const entityId = (isStudent) ? req.body.studentId : req.body.facultyId;
+        const classService = ClassService.getInstance();
+        const assignmentService = AssignmentService.getInstance();
+        const oneClass = await classService.getClass(classId);
+        const members = await classService.getAllStudents(classId);
+        const assignments = await assignmentService.getAllAssignments(classId, isStudent, entityId);
+        const filePath = path.resolve(__dirname, '../web/html/class.html')
+        const html = await ejs.renderFile(filePath, {
+            oneClass: { name: "DSA" },
+            assignments: [
+                {title: "one", difficultyLevel: "EASY"}, 
+                {title: "two", difficultyLevel: "MEDIUM"},
+                {title: "three", difficultyLevel: "HARD"},
+            ],
+            members: [19070124042, 19070124043, 19070124043]    
+        });
+        res.send(html);
+    } catch (err) {
+        if (err instanceof errors.ErrClassNotFound) {
+            res.status(400).json({message: err.message});
+            return;
+        }
+        console.log(err);
+        res.status(500).json({message: messages.MESSAGE_500 });
+    }
+});
 
 export default pageRouter;
