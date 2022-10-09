771eff6f1575bf8a3d4c91dda097fb4b65179763
viveknathani <viveknathani2402@gmail.com>
2022-10-09 16:09:24 +0530

    fix: issues in viewing assignment for faculty

--- src/web/javascript/viewAssignment.js
+++ src/web/javascript/viewAssignment.js
@@ -3,8 +3,9 @@ document.addEventListener("DOMContentLoaded", displayAssignment());
 let assignmentData;
 
 function displayAssignment() {
-   let assignmentId;
-   fetch("/api/assignment?assignmentId=" + assignmentId, {
+   let assignmentId = window.location.pathname.substring('/assignment/'.length);
+   assignmentId = assignmentId.substring(0, assignmentId.indexOf('/view'));
+   fetch(`/api/assignment/${assignmentId}`, {
       method: 'GET',
       headers: {
          Accept: 'application/json',
@@ -20,8 +21,7 @@ function displayAssignment() {
 }
 
 function showAssignment() {
-   document.getElementById("assignmentTitle").innerText = assignmentData.title;
-   document.getElementById("difficulty").innerText = assignmentData.difficultyLevel;
+   document.getElementById("assignmentTitle").innerText = assignmentData.assignment.title;
    if(assignmentData.difficultyLevel == "EASY") {
       document.getElementById("difficulty").style.backgroundColor = "rgb(84, 196, 233)";
    }
@@ -31,11 +31,11 @@ function showAssignment() {
    else if(assignmentData.difficultyLevel == "DIFFICULT") {
       document.getElementById("difficulty").style.backgroundColor = "red";
    }
-   document.getElementById("score").innerText = assignmentData.points + " points";
-   document.getElementById("description").innerText = assignmentData.description;
-   document.getElementById("sampleInput").innerText = assignmentData.sampleInput;
-   document.getElementById("sampleOutput").innerText = assignmentData.sampleOutput;
-   document.getElementById("constraints").innerText = assignmentData.constraints;
-   document.getElementById("timeLimit").innerText = assignmentData.timeLimitSeconds;
-   document.getElementById("memoryLimit").innerText = assignmentData.memoryLimitMB;
+   document.getElementById("score").innerText = assignmentData.assignment.points + " points";
+   document.getElementById("description").innerText = assignmentData.assignment.description;
+   document.getElementById("sampleInput").innerText = assignmentData.assignment.sampleInput;
+   document.getElementById("sampleOutput").innerText = assignmentData.assignment.sampleOutput;
+   document.getElementById("constraints").innerText = assignmentData.assignment.constraints;
+   document.getElementById("timeLimit").innerText = assignmentData.assignment.timeLimitSeconds;
+   document.getElementById("memoryLimit").innerText = assignmentData.assignment.memoryLimitMB;
 }
