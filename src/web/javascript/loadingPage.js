bcc059484d486cdeedf27880686d24eaff5d1833
Ronak-Malkan <ronakmalkan2062001@gmail.com>
2022-10-27 23:19:05 +0530

    fix: optimised loadingPage.js code

--- src/web/javascript/loadingPage.js
+++ src/web/javascript/loadingPage.js
@@ -1,53 +1,15 @@
 const code = document.getElementById("code");
 
+const innerHTMLs = ["<span style='color: black;'>c</span>", "<span style='color: black;'>o</span>", "<span style='color: black;'>n</span>", "<span style='color: black;'>s</span>", "<span style='color: black;'>o</span>", "<span style='color: black;'>l</span>", "<span style='color: black;'>e</span>", "<span style='color: black;'>.</span>", "<span style='color: black;'>l</span>","<span style='color: black;'>o</span>", "<span style='color: black;'>g</span>", "<span style='color: black;'>(</span>", "'", "l", "o", "a", "d", "i", "n", "g", "'", "<span style='color: black;'>)</span>", "<span style='color: black;'>;</span>"];
+
 let time = 200;
 
 code.classList.remove("borderBlink");
-setTimeout(() => code.innerHTML += "<span style='color: black;'>c</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>o</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>n</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>s</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>o</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>l</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>e</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>.</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>l</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>o</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>g</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>(</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "'", time);
-time += 170;
-setTimeout(() => code.innerHTML += "l", time);
-time += 170;
-setTimeout(() => code.innerHTML += "o", time);
-time += 170;
-setTimeout(() => code.innerHTML += "a", time);
-time += 170;
-setTimeout(() => code.innerHTML += "d", time);
-time += 170;
-setTimeout(() => code.innerHTML += "i", time);
-time += 170;
-setTimeout(() => code.innerHTML += "n", time);
-time += 170;
-setTimeout(() => code.innerHTML += "g", time);
-time += 170;
-setTimeout(() => code.innerHTML += "'", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>)</span>", time);
-time += 170;
-setTimeout(() => code.innerHTML += "<span style='color: black;'>;</span>", time);
+for(let i=0; i<innerHTMLs.length-1; i++) {
+   setTimeout(() => code.innerHTML += innerHTMLs[i], time);
+   time += 170;
+}
+setTimeout(() => code.innerHTML += innerHTMLs[innerHTMLs.length-1], time);
 code.classList.add("borderBlink");
 time = 200;
 
@@ -57,51 +19,11 @@ setInterval(writeCode, 5500);
 function writeCode() {
    code.classList.remove("borderBlink");
    code.innerHTML = "";
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>c</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>o</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>n</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>s</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>o</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>l</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>e</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>.</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>l</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>o</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>g</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>(</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "'", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "l", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "o", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "a", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "d", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "i", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "n", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "g", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "'", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>)</span>", time);
-   time += 170;
-   setTimeout(() => code.innerHTML += "<span style='color: black;'>;</span>", time);
+   for(let i=0; i<innerHTMLs.length-1; i++) {
+      setTimeout(() => code.innerHTML += innerHTMLs[i], time);
+      time += 170;
+   }
+   setTimeout(() => code.innerHTML += innerHTMLs[innerHTMLs.length-1], time);
    code.classList.add("borderBlink");
    time = 200;
 }
